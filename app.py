c3f4030e69d22aabb0891ac87c4528156692bd7c
WDoyle123 <will.doyle30@gmail.com>
2022-09-30 21:08:00 +0100

    buggy graph

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
+from cProfile import label
 from fileinput import filename
 import readline
 import matplotlib.pyplot as plt 
 import pandas as pd
 import csv
+import os
 
 
-star1_lum = []
-star1_teff =[]
+count = 0
+x= ("x")
+y= ("y")
+with os.scandir("/home/will/Documents/Astrophysics/stellar-model/data") as it:
+    for entry in it: 
+        with open(entry) as datFile:
+            lum = ([data.split()[1] for data in datFile])
+            lum.pop(0)
+        with open(entry) as datFile:
+            teff = ([data.split()[2] for data in datFile])
+            teff.pop(0)
+        plt.plot(teff,lum, marker = "o", label=entry)
 
-with open("my_file.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        lum = row["logL"]
-        teff = row["logTef"]
-        star1_lum.append(lum)
-        star1_teff.append(teff)
 
-print(star1_lum)
-print(star1_teff)
-
-plt.plot(star1_lum, star1_teff)
+# plt.rcParams["figure.figsize"] = [7.00, 3.50]
+# plt.rcParams["figure.autolayout"] = True
 plt.title("The Physics of Stellar Models")
+plt.legend()
 plt.ylabel("Luminosity, Log L")
 plt.xlabel("Log Teff")
-plt.ylim(0,10)
-plt.xlim(0,10)
 plt.show()
 
 
--- dat_rename.py
+++ dat_rename.py
@@ -2,14 +2,14 @@ import pandas as pd
 import os
 
 count=0
-x = ".csv"
-with os.scandir("/home/will/Documents/Astrophysics/physics-of-stellar-models/data") as it:
+x = ".dat"
+with os.scandir("/home/will/Documents/Astrophysics/stellar-model/data") as it:
     for entry in it:
         name = (str(count))
         count +=1
         if entry.name.endswith(".dat") and entry.is_file():
             read_file = pd.read_csv(entry)
-            read_file.to_csv (name+x, index=None)
+            read_file.to_csv (entry, index=None)
         continue
 
 
