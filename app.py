69e3eb746f5878c5eb547dd220a2115388531201
WDoyle123 <will.doyle30@gmail.com>
2022-10-01 10:54:32 +0100

    Graph showing star tracks of varying solar masses

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-from cProfile import label
-from fileinput import filename
-import readline
 import matplotlib.pyplot as plt 
-import pandas as pd
-import csv
 import os
 
-
-count = 0
-x= ("x")
-y= ("y")
+plt.figure(figsize=(10, 8), dpi=80)
 with os.scandir("/home/will/Documents/Astrophysics/stellar-model/data") as it:
     for entry in it: 
+
         with open(entry) as datFile:
-            lum = ([data.split()[1] for data in datFile])
+            next(datFile)
+            lum = ([float(data.split()[1]) for data in datFile])
             lum.pop(0)
+
         with open(entry) as datFile:
-            teff = ([data.split()[2] for data in datFile])
+            next(datFile)
+            teff = ([float(data.split()[2]) for data in datFile])
             teff.pop(0)
-        plt.plot(teff,lum, marker = "o", label=entry)
 
+        plt.plot(teff,lum, marker =".", ls="dotted" ,label=entry.name)
 
-# plt.rcParams["figure.figsize"] = [7.00, 3.50]
-# plt.rcParams["figure.autolayout"] = True
-plt.title("The Physics of Stellar Models")
-plt.legend()
+plt.title("The Physics of Stellar Models - William Doyle")
 plt.ylabel("Luminosity, Log L")
 plt.xlabel("Log Teff")
-plt.show()
+ax = plt.gca()
+ax.set_xlim(ax.get_xlim()[::-1])
 
+plt.legend()
+plt.show()
+plt.close()
 
 
